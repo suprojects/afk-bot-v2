54c5fbd03a59270585397ac5285e52e78c296dc5
Alpha <sudo.alpha@outlook.com>
2022-03-31 22:17:27 +0530

    database support

---
+++ database/tgusers.py
@@ -0,0 +1,53 @@
+from pymongo import ReturnDocument
+from datetime import datetime
+
+from database import db
+
+tgusers = db["tgusers"]
+
+def online_user(from_user):
+    param = tgusers.find_one_and_update(
+            {"id": from_user.id},
+            {
+                "$set": {
+                    "username": from_user.username,
+                    "firstname": from_user.first_name,
+                    "lastname": from_user.last_name,
+                    "seen": datetime.now(),
+                    "afk_status": False,
+                }
+            },
+            upsert=True,
+            return_document=ReturnDocument.BEFORE
+        )
+    
+    return param
+
+def afked(from_user, reason = None):
+    tgusers.update_one(
+        {"id": from_user.id},
+        {
+            "$set": {
+                "afk_status": True,
+                "reason": reason,
+            }
+        },
+        upsert=True,
+    )
+
+def new_botuser(userid):
+    tgusers.update_one(
+        {"id": from_user.id},
+        {
+            "$set": {
+                "botuser": True,
+            }
+        },
+        upsert=True,
+    )
+
+def if_afk(userid):
+    return tgusers.find_one({'id': userid})
+
+def bot_users():
+    return list(tgusers.find({}, {'id': 1, "username": 1, "firstname": 1, "lastname": 1}))
