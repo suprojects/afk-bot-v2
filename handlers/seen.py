7d6261a03751a6b9d0dcc89ebac1ba9e24908b5b
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2022-04-08 18:55:08 +0530

    Fully implemented private settings

--- handlers/seen.py
+++ handlers/seen.py
@@ -10,33 +10,46 @@ async def seenViewer(client, m):
     param = m.command[1:]
     
     if param:
-        mentioned = userFinder.find(param[0])
-        if mentioned:
-            if mentioned.get('afk_status', False):
-                reply_message = """
-{mention} is AFK
-AFK since: {elapsed}
-Reason: {reason}
-""".format(
-    mention=f"[{mentioned['first_name']}](tg://user?id={mentioned['id']})",
-    elapsed=timehelper.readableTime(timehelper.getDuration(mentioned['seen'])),
-    reason='`' + mentioned['reason'] + '`' if mentioned['reason'] else "Not specified",
-)
-
-                if mentioned.get('afk_media', False): 
-                    if mentioned['afk_media']['type'] == 'video': await m.reply_video(video = mentioned['afk_media']['id'], caption = reply_message, parse_mode = 'markdown')
-                    elif mentioned['afk_media']['type'] == 'photo': await m.reply_photo(photo = mentioned['afk_media']['id'], caption = reply_message, parse_mode = 'markdown')
-                    else: await m.reply(reply_message, parse_mode = 'markdown')
-                else: await m.reply(reply_message, parse_mode = 'markdown')
         
-            elif not mentioned.get('seen_privacy', False):
-                await m.reply("""
-Last seen {mention} before {elapsed}      
-""".format(
-        mention=f"[{mentioned['first_name']}](tg://user?id={mentioned['id']})",
-        elapsed=timehelper.readableTime(timehelper.getDuration(mentioned['seen'])),
-    ))
+        mentioned = userFinder.find(param[0])
+        
+        if mentioned:
+            
+            if mentioned.get('seen', False): 
                 
+                if mentioned.get('afk_status', False):
+                    
+                    reply_message = """
+                        {mention} is AFK
+                        AFK since: {elapsed}
+                        Reason: {reason}
+                        """.format(
+                            mention=f"[{mentioned['first_name']}](tg://user?id={mentioned['id']})",
+                            elapsed=timehelper.readableTime(timehelper.getDuration(mentioned['seen'])),
+                            reason='`' + mentioned['reason'] + '`' if mentioned['reason'] else "Not specified",
+                        )
+
+                    if mentioned.get('afk_media', False): 
+                        if mentioned['afk_media']['type'] == 'video': await m.reply_video(video = mentioned['afk_media']['id'], caption = reply_message, parse_mode = 'markdown')
+                        
+                        elif mentioned['afk_media']['type'] == 'photo': await m.reply_photo(photo = mentioned['afk_media']['id'], caption = reply_message, parse_mode = 'markdown')
+                        
+                        else: await m.reply(reply_message, parse_mode = 'markdown')
+                    
+                    else: await m.reply(reply_message, parse_mode = 'markdown')
+            
+                elif not mentioned.get('privacy_seen', False):
+                    
+                    await m.reply("""
+                        Last seen {mention} before {elapsed}      
+                        """.format(
+                                mention=f"[{mentioned['first_name']}](tg://user?id={mentioned['id']})",
+                                elapsed=timehelper.readableTime(timehelper.getDuration(mentioned['seen'])),
+                            )
+                        )
+                  
+                else: await m.reply('`{mentioned}` not seen before'.format(mentioned = param[0]), parse_mode = 'markdown')
+  
             else: await m.reply('`{mentioned}` not seen before'.format(mentioned = param[0]), parse_mode = 'markdown')
             
         else: await m.reply('`{mentioned}` not seen before'.format(mentioned = param[0]), parse_mode = 'markdown')
--- handlers/settings.py
+++ handlers/settings.py
@@ -1,61 +1,135 @@
 from pyrogram import Client, filters
+
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
 from database import tgusers, pvtsettings
+
 from utils import settingshelper
 from distutils.util import strtobool
 
+
 settings_text = """
+
 Current settings for {mention}:
 
-Time hider: `{privacy_time}`
-Seen hider: `{privacy_seen}`
-Mention Logger: `{mention_log}`
-Language: `{lang}`
+Time hider: **{privacy_time}**
+Seen hider: **{privacy_seen}**
+Mention Logger: **{mention_log}**
 
-__Click the buttons to learn about the settings available__
+Language: **{lang}**
+
+__Click the buttons to learn about the settings__
+
 """
 
 
+
 @Client.on_message(filters.command('settings') & filters.private)
+
 async def settings_pvt(c,m):
     settings = tgusers.find_by_id(m.from_user.id)
-    
-    buttons = settingshelper.pvt_settings_buttons(settingshelper.btn_maker(settings))
+
+    buttons = settingshelper.btn_maker(settingshelper.pvt_settings_btns(settings))
     
     await m.reply(settings_text.format(
+
         mention=f'[{m.from_user.first_name}](tg://user?id={m.from_user.id})',
-        privacy_time=settings.get('privacy_time', False),
-        privacy_seen=settings.get('privacy_seen', False),
-        mention_log=settings.get('mention_log', False),
+        privacy_time=("Enabled" if settings.get('privacy_time', False) else "Disabled"),
+        privacy_seen=("Enabled" if settings.get('privacy_seen', False) else "Disabled"),
+        mention_log= ("Enabled" if settings.get('mention_log', False) else "Disabled"),
         lang=settings.get('lang', 'en'),
+
 ), parse_mode='markdown', reply_markup=InlineKeyboardMarkup(buttons))
-    
+
+
 @Client.on_callback_query(filters.regex('^privacy_time'))
+
 async def timeprivacy(c,m):
+
     param = m.data.split('_')[2:][0]
 
+
     if param == 'help': await m.answer("Hides the duration you were AFK in reply messages", show_alert=True)
+
     else: 
-        pvtsettings.setting(m.from_user, 'privacy_time', bool(strtobool(param)))
-        await m.answer('Set Time hider to {data}'.format(data=param), show_alert=True)
-        await m.edit
+        
+        settings = pvtsettings.setting(m.from_user, 'privacy_time', bool(strtobool(param)))
+        
+        buttons = settingshelper.btn_maker(settingshelper.pvt_settings_btns(settings))
+        
+        await m.answer('{data} Time hider'.format(data="Enabled" if bool(strtobool(param)) else "Disabled"), show_alert=True)
+        
+        await c.edit_message_text(
+            chat_id = m.from_user.id,
+            message_id = m.message.message_id,
+            text=settings_text.format(
+                mention=f'[{m.from_user.first_name}](tg://user?id={m.from_user.id})',
+                privacy_time=settings.get('privacy_time', False),
+                privacy_seen=settings.get('privacy_seen', False),
+                mention_log=settings.get('mention_log', False),
+                lang=settings.get('lang', 'en'),
+                
+            ), parse_mode='markdown', reply_markup=InlineKeyboardMarkup(buttons)
+        )
 
 
 @Client.on_callback_query(filters.regex('^privacy_seen'))
+
 async def seenprivacy(c,m):
+
     param = m.data.split('_')[2:][0]
 
+
     if param == 'help': await m.answer("Disables /seen command to be used on your account", show_alert=True)
+
     else: 
-        pvtsettings.setting(m.from_user, 'privacy_seen', bool(strtobool(param)))
-        await m.answer('Set Seen Hider to {data}'.format(data=param), show_alert=True)
+
+        settings = pvtsettings.setting(m.from_user, 'privacy_seen', bool(strtobool(param)))
+        
+        buttons = settingshelper.btn_maker(settingshelper.pvt_settings_btns(settings))
+        
+        await m.answer('{data} Seen hider'.format(data="Enabled" if bool(strtobool(param)) else "Disabled"), show_alert=True)
+        
+        await c.edit_message_text(
+            chat_id = m.from_user.id,
+            message_id = m.message.message_id,
+            text=settings_text.format(
+                mention=f'[{m.from_user.first_name}](tg://user?id={m.from_user.id})',
+                privacy_time=settings.get('privacy_time', False),
+                privacy_seen=settings.get('privacy_seen', False),
+                mention_log=settings.get('mention_log', False),
+                lang=settings.get('lang', 'en'),
+                
+            ), parse_mode='markdown', reply_markup=InlineKeyboardMarkup(buttons)
+        )
 
 
 @Client.on_callback_query(filters.regex('^mention_log'))
+
 async def mentionlogger(c,m):
+
     param = m.data.split('_')[2:][0]
 
+
     if param == 'help': await m.answer("Sends you a PM for any mentions you get", show_alert=True)
+
     else: 
-        pvtsettings.setting(m.from_user, 'mention_log', bool(strtobool(param)))
-        await m.answer('Set Mention Logger to {data}'.format(data=param), show_alert=True)
+
+        settings = pvtsettings.setting(m.from_user, 'mention_log', bool(strtobool(param)))
+        
+        buttons = settingshelper.btn_maker(settingshelper.pvt_settings_btns(settings))
+        
+        await m.answer('{data} Mention Logger'.format(data="Enabled" if bool(strtobool(param)) else "Disabled"), show_alert=True)
+        
+        await c.edit_message_text(
+            chat_id = m.from_user.id,
+            message_id = m.message.message_id,
+            text=settings_text.format(
+                mention=f'[{m.from_user.first_name}](tg://user?id={m.from_user.id})',
+                privacy_time=settings.get('privacy_time', False),
+                privacy_seen=settings.get('privacy_seen', False),
+                mention_log=settings.get('mention_log', False),
+                lang=settings.get('lang', 'en'),
+                
+            ), parse_mode='markdown', reply_markup=InlineKeyboardMarkup(buttons)
+        )
