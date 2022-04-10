b95e3e9d02aaa407775b8e27444b7b39a2a877ad
Alpha <sudo.alpha@outlook.com>
2022-04-10 21:21:56 +0530

    added day support in elapsed duration

--- utils/timehelper.py
+++ utils/timehelper.py
@@ -6,6 +6,8 @@ def getDuration(then):
     
     time = dict()
     
+    time['d'] = elapsedTime // 86400
+    elapsedTime %= 86400
     time['h'] = elapsedTime // 3600
     elapsedTime %= 3600
     time['m'] = elapsedTime // 60
@@ -19,23 +21,23 @@ def readableTime(time):
     readable = ""
 
     if time.get('mnth', False):
-       readable += str(time['mnth'] + f"second{'mnth' if time['mnth'] != 1 else ''} ")
+       readable += str(time['mnth']) + ' ' + f"second{'mnth' if time['mnth'] != 1 else ''}, "
        
     if time.get('w', False):
-        readable += str(time['w'] + f"week{'s' if time['w'] != 1 else ''} ")
+        readable += str(time['w']) + ' ' + f"week{'s' if time['w'] != 1 else ''}, "
         
     if time.get('d', False):
-        readable += str(time['d'] + f"day{'s' if time['d'] != 1 else ''} ")
+        readable += str(time['d']) + ' ' + f"day{'s' if time['d'] != 1 else ''}, "
         
     if time.get('h', False):
-        readable += str(time['h']) +' ' + f"hour{'s' if time['h'] != 1 else ' '} "
+        readable += str(time['h']) + ' ' + f"hour{'s' if time['h'] != 1 else ' '}, "
         
     if time.get('m', False):
-        readable += str(time['m']) + ' ' + f"minute{'s' if time['m'] != 1 else ' '} "
+        readable += str(time['m']) + ' ' + f"minute{'s' if time['m'] != 1 else ' '}, "
         
     if time.get('s', False):
-        readable += str(time['s']) + ' ' + f"second{'s' if time['s'] != 1 else ' '} "
+        readable += str(time['s']) + ' ' + f"second{'s' if time['s'] != 1 else ' '}, "
         
-    readable = readable.strip()
+    readable = readable.rstrip(', ')
     
     return readable
