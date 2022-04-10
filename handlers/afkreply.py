6fde1a1d9d43b3382d58df25bbc5341a731ba694
Alpha <sudo.alpha@outlook.com>
2022-04-10 18:37:36 +0530

    added check if member in group

--- handlers/afkreply.py
+++ handlers/afkreply.py
@@ -2,6 +2,7 @@ from database import tgusers
 
 from pyrogram import Client, filters
 from pyrogram.types import ChatMember
+from pyrogram.errors import exceptions
 
 from utils import timehelper
 from utils.formatutils import autobool
@@ -13,14 +14,13 @@ import re
 @Client.on_message(filters.group & ~filters.regex('^/'))
 async def afk_replier(c, m):
     
-    await noafk.noafk(c, m)
-    
-    #if not re.search('#afk', m.text): await noafk.noafk(c, m)
+    if m.text:
+        if not re.search('#afk', m.text):
+            await noafk.noafk(c, m)
+
 
     if m.reply_to_message:
-        
-        status = tgusers.find_by_id(m.reply_to_message.from_user.id)
-    
+        status = tgusers.find_by_id(m.reply_to_message.from_user.id)   
 
     elif m.entities:
         
@@ -35,20 +35,28 @@ async def afk_replier(c, m):
     
 
     if status.get('afk_status', False):
+         
+        try:
+            member = await c.get_chat_member(m.chat.id, status['id'])
             
-        if True:
-            
-            reply_message = ("{mention} is AFK{afk_since}\nReason: {reason}".format(
-                    mention=f"[{status['first_name']}](tg://user?id={status['id']})",
-                    afk_since=f"\nAFK since: {timehelper.readableTime(timehelper.getDuration(status['seen']))}" if not autobool(status.get('privacy_time', False))['bool'] else "",
-                    reason='`' + status['reason'] + '`' if status['reason'] else "Not specified",
+        except exceptions.bad_request_400.UserNotParticipant: 
+            return
+        
+        else:
+        
+            if member.status == 'creator' or member.status == 'administrator' or member.status == 'member':
+                
+                reply_message = ("{mention} is AFK{afk_since}\nReason: {reason}".format(
+                        mention=f"[{status['first_name']}](tg://user?id={status['id']})",
+                        afk_since=f"\nAFK since: {timehelper.readableTime(timehelper.getDuration(status['seen']))}" if not autobool(status.get('privacy_time', False))['bool'] else "",
+                        reason='`' + status['reason'] + '`' if status['reason'] else "Not specified",
+                    )
                 )
-            )
 
-            if status.get('afk_media', False):
-                
-                if status['afk_media']['type'] == 'video': await m.reply_video(video = status['afk_media']['id'], caption = reply_message, parse_mode = 'markdown')
-                elif status['afk_media']['type'] == 'photo': await m.reply_photo(photo = status['afk_media']['id'], caption = reply_message, parse_mode = 'markdown')
+                if status.get('afk_media', False):
+                    
+                    if status['afk_media']['type'] == 'video': await m.reply_video(video = status['afk_media']['id'], caption = reply_message, parse_mode = 'markdown')
+                    elif status['afk_media']['type'] == 'photo': await m.reply_photo(photo = status['afk_media']['id'], caption = reply_message, parse_mode = 'markdown')
+                    else: await m.reply(reply_message, parse_mode = 'markdown')
+                    
                 else: await m.reply(reply_message, parse_mode = 'markdown')
-                
-            else: await m.reply(reply_message, parse_mode = 'markdown')
