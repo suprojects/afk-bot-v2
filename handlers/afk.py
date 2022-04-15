23a335267b0fc3fe469e1af617765117f61187f5
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2022-04-15 14:06:18 +0530

    implemented auto delete using apscheuler

--- handlers/afk.py
+++ handlers/afk.py
@@ -1,27 +1,46 @@
 from pyrogram import Client, filters
-from database import tgusers
+
+from database import tgusers, groupsettings
+
+from utils import autoDelete
+
 
 @Client.on_message(filters.command('afk') & ~filters.edited)
-async def afk(client, message):
+async def afk(c, m):
     
     reason = None
     afk_media = None
     
-    if message.command[1:]: reason = str(" ".join(message.command[1:]))
+    if m.command[1:]: reason = str(" ".join(m.command[1:]))
     
-    if message.photo: afk_media = {'id': message.photo.file_id, 'type': 'photo'}
-    if message.video:
-        if message.video.duration <= 30: afk_media = {'id': message.video.file_id, 'type': 'video'}
-        else: 
-            await message.reply('Duration of the video must be lesser than 30 seconds')
-            return
+    if m.photo:
+        
+        afk_media = {'id': m.photo.file_id, 'type': 'photo'}
+        
+    if m.video:
+        
+        if m.video.duration <= 30:
             
-    tgusers.afked(message.from_user, reason, afk_media)
+            afk_media = {'id': m.video.file_id, 'type': 'video'}
+            
+        else: 
+            
+            await m.reply('Duration of the video must be lesser than 30 seconds')
+            return
 
-    await message.reply("""
-{mention} is now AFK
-Reason: {reason}
-""".format(
-    mention=f'[{message.from_user.first_name}](tg://user?id={message.from_user.id})',
-    reason='`' + reason + '`' if reason else "Not specified",
-), parse_mode = 'markdown')
+
+    tgusers.afked(m.from_user, reason, afk_media)
+
+    x = await m.reply("{mention} is now AFK\nReason: {reason}".format(
+            mention=f'[{m.from_user.first_name}](tg://user?id={m.from_user.id})',
+            reason='`' + reason + '`' if reason else "Not specified",
+        ), parse_mode = 'markdown')
+
+
+    if m.chat.type != 'private':
+        
+        group_settings = groupsettings.find_by_id(m.chat.id)
+
+        if group_settings.get('cleanup', 'false') != 'false':
+            
+            autoDelete.newDeleteJob(chat_id = m.chat.id, message_id = x.message_id, delete_delay = group_settings['cleanup'], delete_command=group_settings.get('cleanup_commands', False), command_id=x.reply_to_message_id)
--- handlers/noafk.py
+++ handlers/noafk.py
@@ -1,6 +1,8 @@
 from pyrogram import Client, filters
-from database import tgusers
-from utils import timehelper
+
+from database import tgusers, groupsettings
+
+from utils import timehelper, autoDelete
 from utils.formatutils import autobool
 
 
@@ -8,14 +10,23 @@ from utils.formatutils import autobool
 async def noafk(c, m):
 
     status = tgusers.online_user(m.from_user)
-    
 
-    if status:
-        if status.get('afk_status', False):
-            elapsed = timehelper.readableTime(timehelper.getDuration(status['seen']))
+
+    if status and status.get('afk_status', False):
+
+        elapsed = timehelper.readableTime(timehelper.getDuration(status['seen']))
+        
+        x = await m.reply("{mention} is no longer AFK{afk_since}\nReason: {reason}".format(
+                mention=f'[{m.from_user.first_name}](tg://user?id={m.from_user.id})',
+                afk_since=f"\nAFK since: {elapsed}" if not autobool(status.get('privacy_time', False))['bool'] else "",
+                reason='`' + status['reason'] + '`' if status['reason'] else "Not specified",
+            ), parse_mode = 'markdown')
+
+
+        if m.chat.type != 'private':
             
-            msg = await m.reply("{mention} is no longer AFK{afk_since}\nReason: {reason}".format(
-                    mention=f'[{m.from_user.first_name}](tg://user?id={m.from_user.id})',
-                    afk_since=f"\nAFK since: {elapsed}" if not autobool(status.get('privacy_time', False))['bool'] else "",
-                    reason='`' + status['reason'] + '`' if status['reason'] else "Not specified",
-                ), parse_mode = 'markdown')
+            group_settings = groupsettings.find_by_id(m.chat.id)
+            
+            if group_settings.get('cleanup', 'false') != 'false':
+                        
+                autoDelete.newDeleteJob(chat_id = m.chat.id, message_id = x.message_id, delete_delay = group_settings['cleanup'])
